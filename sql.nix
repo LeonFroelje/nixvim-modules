368f23f6aa4f18c7658c1df555d3b73082e280af
leon froelje <Leon.froelje@uni-bremen.de>
2026-07-03 00:03:16 +0200

    sql

--- sql.nix
+++ sql.nix
@@ -8,11 +8,25 @@ let
   cfg = config.sqlConfig;
 in
 {
-  options.sqlConfig.enable = lib.mkEnableOption "sql support";
+  options.sqlConfig = {
+    enable = lib.mkEnableOption "sql support";
+    configFile = lib.mkOption {
+      type = lib.types.nullOr lib.types.path;
+      default = null;
+
+    };
+
+  };
+
   config = lib.mkIf cfg.enable {
     plugins.treesitter.grammarPackages = [ pkgs.vimPlugins.nvim-treesitter.builtGrammars.sql ];
     plugins.lsp.servers.sqls = {
       enable = true;
+      cmd = [
+        "${pkgs.sqls}"
+        "-config"
+        cfg.configFile
+      ];
     };
     # plugins.conform-nvim.settings.formatters_by_ft.rust = [ "rustfmt" ];
   };
