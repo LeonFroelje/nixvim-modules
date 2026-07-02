68f3f85c902494ac91b6101d1719980d008ac9b5
leon froelje <Leon.froelje@uni-bremen.de>
2026-07-02 23:47:13 +0200

    sql

--- sql.nix
+++ sql.nix
@@ -11,7 +11,7 @@ in
   options.sqlConfig.enable = lib.mkEnableOption "sql support";
   config = lib.mkIf cfg.enable {
     plugins.treesitter.grammarPackages = [ pkgs.vimPlugins.nvim-treesitter.builtGrammars.sql ];
-    plugins.lsp.servers.sqlls = {
+    plugins.lsp.servers.sqls = {
       enable = true;
     };
     # plugins.conform-nvim.settings.formatters_by_ft.rust = [ "rustfmt" ];
